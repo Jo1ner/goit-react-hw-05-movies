4828e38382d120589e3361931157cbbe8d2bd407
Jo1ner <semdimas@gmail.com>
2023-11-10 11:52:29 +0200

    add components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
+// ca36f092d65c5702293abafc88e9f6b7;
+
+import { NavLink } from 'react-router-dom';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <header>
+        <NavLink className="header-nav-link" to="/">
+          Home
+        </NavLink>
+        <NavLink className="header-nav-link" to="/movies">
+          Movies
+        </NavLink>
+      </header>
     </div>
   );
 };
